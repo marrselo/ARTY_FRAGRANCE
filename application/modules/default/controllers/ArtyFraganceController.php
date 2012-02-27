2e458a8d250fcaad8ea6ec0f9953c7690b25c3ef
nazart <nazartj@gmail.com>
2012-02-27 00:47:27 -0500

    cambios nazart

--- application/modules/default/controllers/ArtyFraganceController.php
+++ application/modules/default/controllers/ArtyFraganceController.php
@@ -4,19 +4,29 @@ class Default_ArtyFraganceController extends ZExtraLib_Controller_Action {
 
     public function init() {
         parent::init();
+        $arrayMenu = array(
+            'submenu1' => array('nombre' => 'presentacion', 'ruta' => ''),
+            'submenu2' => array('nombre' => 'submenu1', 'ruta' => 'submenu1'),
+            'submenu3' => array('nombre' => 'submenu1', 'ruta' => 'submenu1'),
+            'submenu4' => array('nombre' => 'submenu1', 'ruta' => 'submenu1')
+        );
+        
+        $arrayMenu = $this->loadMenuIdioma($this->_params['lang'], 4);
+        $this->view->menuIzquierda = $arrayMenu;
+        
     }
 
     public function indexAction() {
         $this->view->classBody = 'lyt_news_precaution';
         $this->view->itemSelect = 'submenu1';
-        
-        $arrayMenu = array(
-            'submenu1' => array('nombre' => 'presentacion', 'ruta' => 'presentacion'),
-            'submenu2' => array('nombre' => 'submenu1', 'ruta' => 'submenu1'),
-            'submenu3' => array('nombre' => 'submenu1', 'ruta' => 'submenu1'),
-            'submenu4' => array('nombre' => 'submenu1', 'ruta' => 'submenu1')
-                );
-        $this->view->menuIzquierda = $arrayMenu;
+    }
+
+    public function moduleContentAction() {
+        //echo "estoy ejemplo";
+    }
+    
+    public function ejemploAction() {
+    echo "estoy ejemplo";
     }
 
 }
