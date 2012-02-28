2df011d465cb3d7786a658832b19d090bef9c857
marcelo <marrselo@gmail.com>
2012-02-28 01:08:13 -0500

    marrselo

--- application/forms/FormLogin.php
+++ application/forms/FormLogin.php
@@ -1,11 +1,12 @@
 <?php
-
 class Application_Form_FormLogin extends Zend_Form
 {
     public function init() {
             $this->setMethod('Post');
             $this->addElement(new Zend_Form_Element_Text('login',array('required'=> true,'label'=>'Correo')));
+            $this->getElement('login')->setAttrib('class','login');
             $this->addElement(new Zend_Form_Element_Password('password',array('required'=> true,'label'=>'Password')));
+            $this->getElement('password')->setAttrib('class','login');
             $this->addElement(new Zend_Form_Element_Submit('Enviar'));
             return $this;
     }
