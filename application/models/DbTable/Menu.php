1e8edd54347739716dc3a5649039a90487ca9ca9
marcelo <marrselo@gmail.com>
2012-02-28 21:57:46 -0500

    marrselo

--- application/models/DbTable/Menu.php
+++ application/models/DbTable/Menu.php
@@ -1,15 +1,5 @@
 <?php
 
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- * Description of Idioma
- *
- * @author Laptop
- */
 class Application_Model_DbTable_Menu extends ZExtraLib_Db_Table
 {
     protected  $_name = "menu";
--- application/models/DbTable/MenuBase.php
+++ application/models/DbTable/MenuBase.php
@@ -1,15 +1,5 @@
 <?php
 
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- * Description of Idioma
- *
- * @author Laptop
- */
 class Application_Model_DbTable_MenuBase extends ZExtraLib_Db_Table
 {
     protected  $_name = "menubase";
