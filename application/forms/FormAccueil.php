256f0ee7d0a4e6ba26ee40de429c50c521aff9ee
marcelo <marrselo@gmail.com>
2012-02-29 23:32:51 -0500

    push geniaaaaaaaaaaal

--- application/forms/FormAccueil.php
+++ application/forms/FormAccueil.php
@@ -4,8 +4,14 @@ class Application_Form_Accueil extends Application_Form_FormBase_FormArticulo
     public function init()
     {
         parent::init();
-        
-        
+        $this->getElement('titulo');
+        $this->getElement('ordenArticulo');
+        $this->getElement('parrafo');
+        $this->getElement('fotoPrincipal');
+        $this->getElement('idIdioma');
+        $this->getElement('idEstadoArticulo')->setSeparator('');        
+        $this->getElement('flagPublicar');     
+        $this->getElement('flagPublicar')->setValue('1');        
     }
   
 }
--- application/forms/formBase/formArticulo.php
+++ application/forms/formBase/formArticulo.php
@@ -8,9 +8,12 @@ class Application_Form_FormBase_FormArticulo extends Zend_Form
             $this->addElement(new Zend_Form_Element_Textarea('parrafo',array('required'=>true)));
             $this->addElement(new Zend_Form_Element_Image('fotoPrincipal'));
             $this->addElement(new Zend_Form_Element_Hidden('idIdioma'));
+            $this->addElement(new Zend_Form_Element_Hidden('idArticulo'));
             $this->addElement(new Zend_Form_Element_Hidden('idEstadoArticulo'));            
             $this->addElement(new Zend_Form_Element_Checkbox('flagPublicar'),array("checked" => "checked"));
-            $this->getElement('estado')->setValue('1');
+            $this->addElement(new Zend_Form_Element_Text('tituloDetalle[]'));
+            $this->addElement(new Zend_Form_Element_Text('parrafoDetalle[]'));
+            $this->addElement(new Zend_Form_Element_File('fotoDetalle[]'));
          
     }
 }
