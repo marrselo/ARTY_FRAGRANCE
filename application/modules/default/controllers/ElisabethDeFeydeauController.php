77ff19b39d76447753915ca2faedd857ad1af9ea
nazart <nazartj@gmail.com>
2012-03-01 01:21:13 -0500

    cambios nazart

--- application/modules/default/controllers/ElisabethDeFeydeauController.php
+++ application/modules/default/controllers/ElisabethDeFeydeauController.php
@@ -3,10 +3,31 @@ class Default_ElisabethDeFeydeauController extends ZExtraLib_Controller_Action
 {
     public function init() {
         parent::init();
+        $arrayMenu = $this->loadMenuIdioma($this->_params['lang'], 6);
+        $this->view->menuIzquierda = $arrayMenu;
+        $this->_menu = $arrayMenu;
+        $this->view->classBody = 'lyt_elisafey_bio';
     }
+    
     public function indexAction()
+    {
+        $arrayKey = array_keys($this->_menu);
+        $this->view->itemSelect = $arrayKey[0];
+
+    }
+    public function ouvragesAction()
+    {
+        
+        
+    }
+    public function realisationsAction()
     {
         
     }
+    public function blogEtPhotosAction()
+    {
+        
+    }
+    
 }
 
