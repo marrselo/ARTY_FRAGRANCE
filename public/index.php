891f8806fdd837928b19f7e897b1b0416158ec04
nazart <nazartj@gmail.com>
2012-03-03 20:51:26 -0500

    cambios nazart

--- public/index.php
+++ public/index.php
@@ -17,6 +17,11 @@ set_include_path(implode(PATH_SEPARATOR, array(
 /** Zend_Application */
 require_once 'Zend/Application.php';
 
+require_once('../library/ZExtraLib/PageCache.php');
+require_once 'Zend/Auth.php';
+if (PHP_SAPI !== 'cli' && !Zend_Auth::getInstance()->hasIdentity()) {
+    ZExtraLib_PageCache::getCache()->start();
+}
 // Create application, bootstrap, and run
 $application = new Zend_Application(
     APPLICATION_ENV,
