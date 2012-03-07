8019e2d0a834045af5b4e6beba68f5d66b1329e2
JMIKE <JMIKE@TARAZONA.(none)>
2012-03-07 11:45:14 -0500

    Actualizando I

--- application/modules/admin/controllers/jsonController.php
+++ application/modules/admin/controllers/jsonController.php
@@ -4,13 +4,18 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-class Admin_SitesController extends ZExtraLib_Controller_Action {
+class Admin_JsonController extends ZExtraLib_Controller_Action {
      public function jsonAction(){
         $this->_helper->layout->disableLayout();
         $this->_helper->viewRenderer->setNoRender();
         $select = $this->_helper->DBAdapter()->select();	    			
         $case = $this->getRequest()->getParam('case',0);
         
-        
+        switch ($case):
+            
+            case 'getEditMenu':
+                
+                break;
+        endswitch;
      }
 }
