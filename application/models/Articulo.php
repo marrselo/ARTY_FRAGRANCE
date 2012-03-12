a1e3d5acaa8699869e0761595aacfbf70973ef3d
James <james.otiniano@gmail.com>
2012-03-12 00:59:42 -0500

    ...

--- application/models/Articulo.php
+++ application/models/Articulo.php
@@ -9,16 +9,24 @@ class Application_Model_Articulo extends ZExtraLib_Model {
         $this->_articulo = new Application_Model_DbTable_Articulo();        
     }
     
-    function listarArticulo($idmenu) {
+    function listarArticulo($idmenu, $est = NULL) {
+        if($est)
         $db = $this->_articulo
                 ->getAdapter()
                 ->select()
                 ->from(array($this->_articulo->getName()))
                 ->where('idMenu = ? ', $idmenu)
-                //->where('idEstadoArticulo = ? ', 1);
+                ->where('idEstadoArticulo = ? ', 1)
                 ;
-        
-        $result = $db->query()->fetchAll();
+        else
+            $db = $this->_articulo
+                ->getAdapter()
+                ->select()
+                ->from(array($this->_articulo->getName()))
+                ->where('idMenu = ? ', $idmenu)
+                //->where('idEstadoArticulo = ? ', 1)
+                ;
+       $result = $db->query()->fetchAll();
         
         return $result;
     }
--- application/modules/default/controllers/CollectionsController.php
+++ application/modules/default/controllers/CollectionsController.php
@@ -3,9 +3,20 @@ class Default_CollectionsController extends ZExtraLib_Controller_Action
 {
     public function init() {
         parent::init();
+
     }
     public function indexAction()
-    {
+    {           
+        $this->params = $this->_getAllParams();        
+        $idIdioma =$this->params["idiomaAllSelect"]["idIdioma"];
+        
+        $articulo = new Application_Model_Articulo();
+        $producto = new Application_Model_DetalleArticulo();
+        
+        $menu = new Application_Model_Menu();
+        $idMenu = $menu->buscaMenu(3, $idIdioma);
+        $this->view->articulo = $articulo->listarArticulo($idMenu['idMenu'], 1);
+        $this->view->producto = $producto->listProductos();
         
     }
 }
