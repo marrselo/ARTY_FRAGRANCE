08496d253b0081dd58064741a2167eaf0c4ec472
unknown <usuario@espinoza-e759a5.(none)>
2012-03-21 23:37:25 -0500

    mensaje de log

---
+++ application/modules/admin/controllers/DemoEditorController.php
@@ -0,0 +1,11 @@
+<?php
+
+class Admin_DemoEditorController extends ZExtraLib_Controller_Action
+{
+    public function init() {
+        parent::init();
+    }
+    public function indexAction()
+    {
+    }
+}
